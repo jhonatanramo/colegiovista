24031c2d7faadcaa588b5b207bcb54cbb9af1093
jhonatanramo <jhonatanleonel666@gmai,com>
2025-11-20 22:02:49 -0400

    erro

--- src/api.js
+++ src/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://colegiologica.onrender.com/",
 });
+//dxcsdcsdc
 // http://127.0.0.1:8000/
 //https://colegiologica.onrender.com/
 api.interceptors.response.use(
